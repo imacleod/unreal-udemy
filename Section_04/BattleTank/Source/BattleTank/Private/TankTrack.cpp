d74a82a05d5b87e7c4dd4236d16e0f788b5031f9
Ian MacLeod <imacleod@users.noreply.github.com>
2017-12-01 13:26:40 -0800

    183 - Begin fix applying sideways force only on ground

--- Section_04/BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ Section_04/BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -9,6 +9,17 @@ UTankTrack::UTankTrack()
 	PrimaryComponentTick.bCanEverTick = true;
 }
 
+void UTankTrack::BeginPlay()
+{
+	Super::BeginPlay();
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
+}
+
+void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Tank track on ground..."));
+}
+
 void UTankTrack::SetThrottle(float Throttle)
 {
 	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
--- Section_04/BattleTank/Source/BattleTank/Public/TankTrack.h
+++ Section_04/BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -16,6 +16,11 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 private:
 	UTankTrack();
 
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+
 	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
 
 public:
