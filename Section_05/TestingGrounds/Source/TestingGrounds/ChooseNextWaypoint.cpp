f85a11f829c199257fd2c686ee53ff7540085ac9
Ian MacLeod <imacleod@users.noreply.github.com>
2017-12-15 12:03:57 -0800

    219 - Read blackboard data in code

--- Section_05/TestingGrounds/Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Section_05/TestingGrounds/Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,8 +1,12 @@
 #include "TestingGrounds.h"
+#include "BehaviorTree/BlackboardComponent.h"
 #include "ChooseNextWaypoint.h"
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto CurrentIndex = BlackboardComp->GetValueAsInt(IndexSelector.SelectedKeyName);
+	UE_LOG(LogTemp, Warning, TEXT("CurrentIndex: %i"), CurrentIndex);
 	return EBTNodeResult::Succeeded;
 }
--- Section_05/TestingGrounds/Source/TestingGrounds/ChooseNextWaypoint.h
+++ Section_05/TestingGrounds/Source/TestingGrounds/ChooseNextWaypoint.h
@@ -13,4 +13,8 @@ class TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 	GENERATED_BODY()
 	
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category="Blackboard")
+	struct FBlackboardKeySelector IndexSelector;
 };
