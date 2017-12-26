5f0bd85a7f3e742199df222a118214d64d9b7e17
Ian MacLeod <imacleod@users.noreply.github.com>
2017-12-26 12:50:04 -0800

    251 - Refactor gun out of first person into actor
    Move gun-related code out of first person character and into new gun
    actor class. Create blueprint based on new gun actor class.

---
+++ Section_05/TestingGrounds/Source/TestingGrounds/Weapons/Gun.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Gun.generated.h"
+
+
+UCLASS()
+class TESTINGGROUNDS_API AGun : public AActor
+{
+	GENERATED_BODY()
+
+	/** Gun mesh: 1st person view (seen only by self) */
+	UPROPERTY(VisibleDefaultsOnly, Category=Mesh)
+	class USkeletalMeshComponent* FP_Gun;
+
+	/** Location on gun mesh where projectiles should spawn. */
+	UPROPERTY(VisibleDefaultsOnly, Category=Mesh)
+	class USceneComponent* FP_MuzzleLocation;
+
+protected:
+	// Fires a projectile
+	void OnFire();
+	
+public:	
+	// Sets default values for this actor's properties
+	AGun();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay)
+	UAnimInstance* AnimInstance;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	/** AnimMontage to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay)
+	class UAnimMontage* FireAnimation;
+
+	/** Sound to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay)
+	class USoundBase* FireSound;
+
+	/** Projectile class to spawn */
+	UPROPERTY(EditDefaultsOnly, Category=Projectile)
+	TSubclassOf<class ABallProjectile> ProjectileClass;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+};
