36297321f172aa0a348ff3c44b234107b9416b3b
Ian MacLeod <imacleod@users.noreply.github.com>
2018-01-09 11:20:39 -0800

    292 - Transform random actor locations to world coords

--- Section_05/TestingGrounds/Source/TestingGrounds/Terrain/Tile.cpp
+++ Section_05/TestingGrounds/Source/TestingGrounds/Terrain/Tile.cpp
@@ -15,42 +15,70 @@ ATile::ATile()
 void ATile::BeginPlay()
 {
 	Super::BeginPlay();
-	CastSphere(GetActorLocation(), 300);
-	CastSphere(GetActorLocation() + FVector(0, 0, 1000), 300);
 }
 
-bool ATile::CastSphere(FVector Location, float Radius)
+bool ATile::CanSpawnAtLocation(FVector Location, float Radius)
 {
 	FHitResult HitResult;
+	FVector GlobalLocation = ActorToWorld().TransformPosition(Location);
+
 	bool HasHit = GetWorld()->SweepSingleByChannel(
 		HitResult,
-		Location,
-		Location,
+		GlobalLocation,
+		GlobalLocation,
 		FQuat::Identity,
 		ECollisionChannel::ECC_GameTraceChannel2,  // Our "Spawn" custom trace channel, determined in DefaultEngine.ini
 		FCollisionShape::MakeSphere(Radius)
 	);
 
 	FColor ColorResult = HasHit ? FColor::Red : FColor::Green;
-	DrawDebugCapsule(GetWorld(), Location, 0, Radius, FQuat::Identity, ColorResult, true, 100);
+	DrawDebugCapsule(GetWorld(), GlobalLocation, 0, Radius, FQuat::Identity, ColorResult, true, 100);
 
-	return HasHit;
+	return !HasHit;
 }
 
-void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn)
+// Search for empty location in level/tile that can be used for actor placement
+bool ATile::FindEmptyLocation(FVector& OutLocation, float Radius)
 {
+	const int MAX_ATTEMPTS = 100;
+
 	// Assuming 4000 x 4000 tile with origin in center of rear barrier
 	FVector Min(0, -2000, 0);
 	FVector Max(4000, 2000, 0);
 	FBox Bounds(Min, Max);
 
+	for (size_t i = 0; i < MAX_ATTEMPTS; i++)
+	{
+		FVector RandomLocation = FMath::RandPointInBox(Bounds);
+
+		if (CanSpawnAtLocation(RandomLocation, Radius))
+		{
+			OutLocation = RandomLocation;
+			return true;
+		}
+	}
+	return false;
+}
+
+void ATile::PlaceActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint)
+{
+	AActor* Spawned = GetWorld()->SpawnActor<AActor>(ToSpawn);
+	Spawned->SetActorRelativeLocation(SpawnPoint);
+	Spawned->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, false));  // Don't weld when simulating physics
+}
+
+void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn, float Radius)
+{
 	int32 NumSpawn = FMath::RandRange(MinSpawn, MaxSpawn);
 	for (size_t i = 0; i < NumSpawn; i++)
 	{
-		FVector SpawnPoint = FMath::RandPointInBox(Bounds);
-		AActor* Spawned = GetWorld()->SpawnActor<AActor>(ToSpawn);
-		Spawned->SetActorRelativeLocation(SpawnPoint);
-		Spawned->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, false));  // Don't weld when simulating physics
+		FVector SpawnPoint;
+		bool ValidLocation = FindEmptyLocation(SpawnPoint, Radius);
+
+		if (ValidLocation)
+		{
+			PlaceActor(ToSpawn, SpawnPoint);
+		}
 	}
 }
 
--- Section_05/TestingGrounds/Source/TestingGrounds/Terrain/Tile.h
+++ Section_05/TestingGrounds/Source/TestingGrounds/Terrain/Tile.h
@@ -14,13 +14,17 @@ public:
 
 	// Randomly generate and place level/tile props
 	UFUNCTION(BlueprintCallable, Category="Weapon")
-	void PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn);
+	void PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn=1, int MaxSpawn=1, float Radius=500);
 
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 private:
-	bool CastSphere(FVector Location, float Radius);
+	bool CanSpawnAtLocation(FVector Location, float Radius);
+
+	bool FindEmptyLocation(FVector& OutLocation, float Radius);
+
+	void PlaceActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint);
 
 protected:
 	// Called when the game starts or when spawned
